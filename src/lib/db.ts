6e7038792b5837517055bce540548f47b00465dd
desenvolvimentosoft01 <desenvolvimentosoft01@users.noreply.github.com>
2026-04-14 21:25:34 -0300

    commit

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,6 +4,8 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
   max: 1,
+  connectionTimeoutMillis: 10000,
+  idleTimeoutMillis: 30000,
 });
 
 export async function query(sql: string, params?: any[]) {
