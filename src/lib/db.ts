ba0be6aa095b5f2e2becef8f634aa80a7e187a7a
desenvolvimentosoft01 <desenvolvimentosoft01@users.noreply.github.com>
2026-04-14 21:12:44 -0300

    Commit

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,9 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+  max: 1,
 });
 
 export async function query(sql: string, params?: any[]) {
